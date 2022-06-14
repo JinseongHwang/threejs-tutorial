084fe093c108b1aa970d87c6254b294e43a47e82
Jinseong Hwang <jinseong.dev@gmail.com>
2022-06-14 22:29:12 +0900

    feat: add exporter (incomplete)

--- main.js
+++ main.js
@@ -1,11 +1,28 @@
-/** References\
+/** References
  * - https://threejs.org/docs/index.html
  * - https://github.com/mrdoob/three.js/
  * - https://code.tutsplus.com/ko/tutorials/webgl-with-threejs-basics--net-35688
  * - https://ahnheejong.name/articles/my-first-octahedron/
+ * - https://itecnote.com/tecnote/javascript-how-to-overlay-html-text-buttons-on-three-js/
  */
 
-'use strict';
+const save = (blob, filename) => {
+    const exportBtn = document.getElementById('exportBtn');
+    const form = document.getElementsByTagName('form')[0];
+    form.action = URL.createObjectURL(blob);
+    exportBtn.download = filename;
+}
+
+const saveString = (text, filename) => {
+    save(new Blob([text], {type: 'text/plain'}), filename);
+};
+
+const exportToObj = (scene) => {
+    alert('Run export! (incomplete)');
+    const exporter = new OBJExporter();
+    const result = exporter.parse(scene);
+    saveString(result, 'object.obj');
+};
 
 const width = window.innerWidth;
 const height = window.innerHeight;
@@ -24,7 +41,7 @@ const torus = new THREE.Mesh(torusGeometry, torusMaterial);
 scene.add(torus);
 
 // 카메라
-const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 100);
 camera.position.y = 16;
 camera.position.z = 40;
 camera.lookAt(torus.position);
@@ -32,7 +49,7 @@ scene.add(camera);
 
 // 배경
 const skyboxGeometry = new THREE.BoxGeometry(10000, 10000, 10000);
-const skyboxMaterial = new THREE.MeshBasicMaterial({ color: 0x000000, side: THREE.BackSide });
+const skyboxMaterial = new THREE.MeshBasicMaterial({color: 0x000000, side: THREE.BackSide});
 const skybox = new THREE.Mesh(skyboxGeometry, skyboxMaterial);
 scene.add(skybox);
 
@@ -49,3 +66,8 @@ const speed = 0.003;
     torus.rotation.z -= speed;
     requestAnimationFrame(update);
 }()); // function call with declaration
+
+window.onload = () => {
+    const exportBtn = document.getElementById('exportBtn');
+    exportBtn.addEventListener('click', () => exportToObj(scene));
+}
